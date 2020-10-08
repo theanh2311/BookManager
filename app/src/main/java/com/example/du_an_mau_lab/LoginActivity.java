7b32f07e1d098eb94d2cd0197bbc2bfea5db7027
NTA2K <theanhqm@gmail.com>
2020-10-08 13:30:30 +0700

    update login with facebook

--- app/src/main/java/com/example/du_an_mau_lab/LoginActivity.java
+++ app/src/main/java/com/example/du_an_mau_lab/LoginActivity.java
@@ -6,39 +6,74 @@ import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.appevents.AppEventsLogger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.du_an_mau_lab.model.ListNguoiDungActivity;
 import com.example.du_an_mau_lab.model.ListTheLoaiActivity;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-            try {
-                PackageInfo info = null;
-                try {
-                    info = getPackageManager().getPackageInfo("com.example.du_an_mau_lab", PackageManager.GET_SIGNATURES);
-                }catch (PackageManager.NameNotFoundException e){
-                    e.printStackTrace();
-                }
-                for (Signature signature : info.signatures){
-                    MessageDigest md = MessageDigest.getInstance("SHA");
-                    md.update(signature.toByteArray());
-//                    Log.d("KeyHash: ", Base64.(md.digest(), android.util.Base64.DEFAULT));
-                }
-            }catch (NoSuchAlgorithmException e){
+        setContentView(R.layout.activity_login);
+        String TAG = "SBC";
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                String hashKey = new String(Base64.encode(md.digest(), 0));
+                Log.i(TAG, "printHashKey() Hash Key: " + hashKey);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            Log.e(TAG, "printHashKey()", e);
+        } catch (Exception e) {
+            Log.e(TAG, "printHashKey()", e);
+        }
+        CallbackManager callbackManager = CallbackManager.Factory.create();
+
+
+        LoginButton loginButton =  findViewById(R.id.login_button);
+        loginButton.setReadPermissions("email");
+        // If using in a fragment
+//        loginButton.setFragment(this);
+
+        // Callback registration
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Toast.makeText(LoginActivity.this, "Login thành công", Toast.LENGTH_SHORT).show();
+                Intent intent =  new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
 
             }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
     }
 }
